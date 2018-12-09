e77d251d53028c09d50bf5687331124212a30517
php dev83 <phpdev83@gmail.com>
2018-12-09 19:29:34 +0530

    navigation added

--- server.js
+++ server.js
@@ -1,32 +1,56 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 const port = process.env.PORT || 3000;
-
-hbs.registerPartials(__dirname + '/views/partials');
 var app = express();
-hbs.registerHelper('getCurrentYear', () =>{
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
 
-// app.use((req,res,next)=>{
-//   console.log("hiss");
-// });
-
-app.use(express.static('public'));
-
-app.set('view engine','hbs');
-
-app.get('/',(req, res)=>{
-  res.send("hi")
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
 });
 
-app.get('/about',(req,res)=>{
-  res.render('about.hbs',{
-    'name':'Tarun Narang'
-  })
-})
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
+});
 
-app.listen(port,()=>{
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
